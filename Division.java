7052a6914672a666358019e21df2d1b338270bab
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-07-31 10:51:07 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Division{
+public static void main(String args[]){
+int a,b,result;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter two no.s");
+a=sc.nextInt();
+b=sc.nextInt();
+
+result=a/b;
+System.out.println("Division is:"+result);
+}
+}
