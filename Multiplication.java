53802b7f9d64f0a868914d841d22cd2af44e5f8b
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-07-31 10:53:21 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Multiplication{
+public static void main(String args[]){
+int a,b,result;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter two no.s");
+a=sc.nextInt();
+b=sc.nextInt();
+
+result=a*b;
+System.out.println("Multiplication is:"+result);
+
+}
+}
