344dae3b72af83ba308d5c244f0fb7f13384efed
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-07-31 10:53:46 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Substraction{
+public static void main(String args[]){
+int a,b,result;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter two no.s");
+a=sc.nextInt();
+b=sc.nextInt();
+
+result=a-b;
+System.out.println("Substraction is:"+result);
+
+}
+}
