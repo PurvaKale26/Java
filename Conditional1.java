36281047513101f7d2ed05047b2deafdfb565222
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-07-31 11:03:13 +0530

    Add files via upload

---
+++ Conditional1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Conditional1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int button= sc.nextInt();
+
+        switch(button)
+        {
+            case 1:System.out.print("Hello");
+            break;
+            case 2:System.out.print("Namaste");
+            break;
+            case 3:System.out.print("bonjour");
+            break;
+            default:
+                System.out.print("invalid");
+        }
+    }
+
+    
+}
