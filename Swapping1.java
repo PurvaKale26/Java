30959472811b2781e6919e2543b0b77ce3ff9fa2
PurvaKale26 <purva.kale_24uit@sanjivanicoe.org.in>
2025-07-31 10:54:36 +0530

    Add files via upload

---
+++ Swapping1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Swapping1{
+public static void main(String args[]){
+int a=10;
+int b=20;
+int temp;
+System.out.println("Before swapping");
+System.out.println(a);
+System.out.println(b);
+temp=b;
+b=a;
+a=temp;
+System.out.println("After swapping");
+System.out.println(a);
+System.out.println(b);
+}
+}
+
